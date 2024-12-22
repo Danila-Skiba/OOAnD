092a82bb11fc12cc9abed82c50b9061273afc2d2
Smolovaia Zhanna <zs_e7@mail.ru>
2024-12-22 14:40:41 +0600

    test: add CommandInjectableCommandTests.cs

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/CommandInjectableCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class CommandInjectableCommandTests
+    {
+        [Fact]
+        public void Positive_Execute_CommandInjectableCommand()
+        {
+            var commandMock = new Mock<ICommand>();
+            var injectableCommand = new CommandInjectableCommand();
+            injectableCommand.Inject(commandMock.Object);
+
+            injectableCommand.Execute();
+
+            commandMock.Verify(command => command.Execute(), Times.Once);
+        }
+
+        [Fact]
+        public void Negative_Execute_CommandInjectableCommand()
+        {
+            var injectableCommand = new CommandInjectableCommand();
+
+            Assert.Throws<InvalidOperationException>(() => injectableCommand.Execute());
+        }
+    }
+}
