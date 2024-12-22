6937787b6791a174368f88e79bea35db8fb850d8
Danila-Skiba <fiks11pikvik@gmail.com>
2024-12-23 03:24:27 +0600

    chore: add SendCommand and MacroCommand

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterIoCDependencySendCommand.cs
@@ -0,0 +1,16 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class RegisterIoCDependencySendCommand : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Commands.Send",
+                (object[] args) => new SendCommand((ICommand)args[1], (ICommandReceiver)args[0])
+            ).Execute();
+        }
+    }
+}
