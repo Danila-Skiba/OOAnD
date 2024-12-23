06e6ca72afcd855a540c89328bbe81b62cc77374
tatsivo <t-pudovkina@inbox.ru>
2024-12-23 16:12:48 +0700

    feat: Add StopCommand

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/StopCommand.cs
@@ -0,0 +1,15 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class StopCommand(string objId, string cmdName) : ICommand
+    {
+
+        public void Execute()
+        {
+            Ioc.Resolve<ICommandInjectable>("Game.Object.GetInjectable", objId, cmdName)
+                .Inject(Ioc.Resolve<ICommand>("Commands.Empty"));
+            
+        }
+    }
+}
