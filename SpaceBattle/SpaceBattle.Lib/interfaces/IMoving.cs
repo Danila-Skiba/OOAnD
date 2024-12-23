7ff4bca3a4655359df664cb8102534d4e651460b
Danila-Skiba <fiks11pikvik@gmail.com>
2024-12-23 16:19:02 +0600

    chore: delete IRotating, IMoving

--- SpaceBattle/SpaceBattle.Lib/interfaces/IMoving.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpaceBattle.Lib
-{
-    public interface IMoving
-    {
-        Vector Position { get; set; }
-        Vector Velocity { get; }
-    }
-}
--- SpaceBattle/SpaceBattle.Lib/interfaces/IRotating.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SpaceBattle.Lib;
-
-public interface IRotating
-{
-    Angle CurrentAngle { get; set; }
-    Angle AngleVelocity { get; }
-}
