3133708e440cfb309a760fa232ef88669d65c8ce
tatsivo <t-pudovkina@inbox.ru>
2024-12-23 16:13:31 +0700

    feat: add RegisterIoCDependencyEmptyCommand

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterIoCDependencyEmptyCommand.cs
@@ -0,0 +1,16 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class RegisterIoCDependencyEmptyCommand : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Commands.Empty",
+                (object[] args) => new EmptyCommand()
+            ).Execute();
+        }
+    }
+}
