6319a7181c14a2f4794f1280151c09df2d00cd2b
Danila-Skiba <fiks11pikvik@gmail.com>
2024-12-23 13:29:27 +0600

    feat: add StartCommandActions.cs, RegisterStartCommandActions and tests

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/RegisterStartCommandTests.cs
@@ -0,0 +1,68 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class RegisterIoCDependencyActionsStartTests : IDisposable
+    {
+        public RegisterIoCDependencyActionsStartTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void RegisterIoCDependencyActionsStart_ResolvesDependency()
+        {
+
+            var registerCommand = new RegisterIocDependencyActionsStart();
+            registerCommand.Execute();
+
+            var injectableCommandMock = new Mock<ICommand>();
+            var receiverMock = new Mock<ICommandReceiver>();
+            var gameObject = new Dictionary<string, object>();
+            var order = new Dictionary<string, object>
+            {
+                {"Cmd", injectableCommandMock.Object},
+                {"Receiver", receiverMock.Object},
+                {"Name", "TestCommand"},
+                {"Object", gameObject}
+            };
+
+            var startCommand = Ioc.Resolve<ICommand>("Actions.Start", order);
+            startCommand.Execute();
+
+            Assert.IsType<StartCommand>(startCommand);
+            Assert.True(gameObject.ContainsKey("TestCommand"));
+            receiverMock.Verify(r => r.Receive(It.IsAny<ICommand>()), Times.Once);
+        }
+
+        [Fact]
+        public void Test_RegisterIoCDependencyActionsStart_DependencyNotResolved()
+        {
+            var commandMock = new Mock<ICommand>();
+            var receiverMock = new Mock<ICommandReceiver>();
+            var gameObject = new Dictionary<string, object>();
+            var order = new Dictionary<string, object>
+            {
+                { "Cmd", commandMock.Object },
+                { "Receiver", receiverMock.Object },
+                { "Name", "TestCommand" },
+                { "Object", gameObject }
+            };
+
+            var registerCommand = new RegisterIocDependencyActionsStart();
+
+            Assert.Throws<Exception>(() => Ioc.Resolve<ICommand>("Actions.Start", order));
+            Assert.False(gameObject.ContainsKey("TestCommand"));
+            receiverMock.Verify(r => r.Receive(It.IsAny<ICommand>()), Times.Never);
+        }
+
+        public void Dispose()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+        }
+    }
+}
---
+++ SpaceBattle/SpaceBattle.Lib/Commands/StartCommandAction.cs
@@ -0,0 +1,31 @@
+﻿namespace SpaceBattle.Lib
+{
+    public class StartCommand : ICommand
+    {
+        private readonly ICommand _injectableCommand;
+        private readonly ICommandReceiver _receiver;
+        private readonly IDictionary<string, object> _gameobject;
+        private readonly string _nameCommand;
+        public StartCommand(ICommand injectableCommand, ICommandReceiver receiver, IDictionary<string, object> gameobject, string name)
+        {
+            _gameobject = gameobject;
+            _injectableCommand = injectableCommand;
+            _receiver = receiver;
+            _nameCommand = name;
+        }
+
+        public void Execute()
+        {
+            _gameobject[_nameCommand] = _injectableCommand;
+
+            try
+            {
+                _receiver.Receive(_injectableCommand);
+            }
+            catch (Exception)
+            {
+                _gameobject.Remove(_nameCommand);
+            }
+        }
+    }
+}
