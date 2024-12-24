31095d08bb035a6e64fbfc66ac083934c2737620
Skripa Andrey <stefan.richard.sk@gmail.com>
2024-12-24 21:08:58 +0600

    feat: add MacroCommand.cs and tests

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/MacroCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle.Lib.Commands
+{
+    public class MacroCommand : ICommand
+    {
+        private readonly ICommand[] _commands;
+
+        public MacroCommand(ICommand[] commands)
+        {
+            _commands = commands ?? throw new ArgumentNullException(nameof(commands));
+        }
+
+        public void Execute()
+        {
+            _commands.ToList().ForEach(command => command.Execute());
+        }
+    }
+}
