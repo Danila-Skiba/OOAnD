b8828cfd9896745a093ad7c400fa00bb938d8153
DJsivo <114789776+DJsivo@users.noreply.github.com>
2024-12-25 23:55:06 +0700

    Update SendCommandTests.cs

--- SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/SendCommandTests.cs
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/SendCommandTests.cs
@@ -7,30 +7,24 @@ namespace SpaceBattle.Lib.Tests
         [Fact]
         public void Execute_ShouldPutCommandIntoReceiver()
         {
-            // Arrange
             var cmd = new Mock<ICommand>();
             var receiver = new Mock<ICommandReceiver>();
             var sendCommand = new SendCommand(cmd.Object, receiver.Object);
 
-            // Act
             sendCommand.Execute();
 
-            // Assert
             receiver.Verify(r => r.Receive(cmd.Object), Times.Once);
         }
 
         [Fact]
         public void Execute_ShouldThrowException_WhenReceiverCannotReceiveCommand()
         {
-            // Arrange
             var cmd = new Mock<ICommand>();
             var receiver = new Mock<ICommandReceiver>();
             var sendCommand = new SendCommand(cmd.Object, receiver.Object);
 
-            // Настройка мока для выброса исключения на конкретном аргументе
             receiver.Setup(r => r.Receive(cmd.Object)).Throws(new Exception());
 
-            // Act & Assert
             Assert.Throws<Exception>(() => sendCommand.Execute());
         }
     }
