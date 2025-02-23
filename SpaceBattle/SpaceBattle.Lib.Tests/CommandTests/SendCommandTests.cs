3172283fbe571b4ae4c6148f3edd361076b17802
Smolovaia Zhanna <zs_e7@mail.ru>
2025-02-23 15:40:12 +0600

    fix: restore missing SendCommandTests.cs from main

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/SendCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class SendCommandTest
+    {
+        [Fact]
+        public void Execute_ShouldPutCommandIntoReceiver()
+        {
+            var cmd = new Mock<ICommand>();
+            var receiver = new Mock<ICommandReceiver>();
+            var sendCommand = new SendCommand(cmd.Object, receiver.Object);
+
+            sendCommand.Execute();
+
+            receiver.Verify(r => r.Receive(cmd.Object), Times.Once);
+        }
+
+        [Fact]
+        public void Execute_ShouldThrowException_WhenReceiverCannotReceiveCommand()
+        {
+            var cmd = new Mock<ICommand>();
+            var receiver = new Mock<ICommandReceiver>();
+            var sendCommand = new SendCommand(cmd.Object, receiver.Object);
+
+            receiver.Setup(r => r.Receive(cmd.Object)).Throws(new Exception());
+
+            Assert.Throws<Exception>(() => sendCommand.Execute());
+        }
+    }
+}
+
