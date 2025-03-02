7a479529a1db86514c4d8d98a20834741d1d51da
Smolovaia Zhanna <zs_e7@mail.ru>
2025-03-02 20:56:26 +0600

    feat: add FireCommand.cs and RegisterFireDependencies.cs

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/FireCommand.cs
@@ -0,0 +1,23 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class FireCommand : ICommand
+    {
+        private readonly IFireable _shooter;
+        private string _lastWeaponId;
+        public FireCommand(IFireable shooter)
+        {
+            _shooter = shooter;
+        }
+
+        public void Execute()
+        {
+            var weapon = Ioc.Resolve<IWeapon>("Weapon.Create", _shooter.Position, _shooter.FireDirection);
+            _lastWeaponId = Guid.NewGuid().ToString();
+            Ioc.Resolve<ICommand>("Game.Item.Add", _lastWeaponId, weapon).Execute();
+        }
+
+        public string GetLastWeaponId() { return _lastWeaponId; }
+    }
+}
---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterFireDependencies.cs
@@ -0,0 +1,15 @@
+﻿using App;
+namespace SpaceBattle.Lib
+{
+    public class RegisterFireDependencies : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Commands.Fire", (object[] args) =>
+            {
+                var shooter = Ioc.Resolve<IFireable>("Adapters.IFireableObject", args[0]);
+                return new FireCommand(shooter);
+            }).Execute();
+        }
+    }
+}
