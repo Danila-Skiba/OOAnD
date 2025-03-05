4b269d4c3a3a0f8abc31d827cfb3ea65deb8fd32
no7yao <dad62482@gmail.com>
2025-03-05 05:51:18 +0300

    feat: add IoCRegisterGameOperationTests.cs

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/IoCRegisterGameOperationTests.cs
@@ -0,0 +1,46 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class IoCRegisterGameOperationTests : IDisposable
+    {
+        public IoCRegisterGameOperationTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void RegisterGameTest()
+        {
+            var reg = new IoCRegisterGameOperation();
+
+            reg.Execute();
+
+            var gameReceiver = Ioc.Resolve<object>("Game.Receiver");
+            Assert.IsType<Game>(gameReceiver);
+        }
+
+        [Fact]
+        public void ExecuteCommandsTest()
+        {
+            var reg = new IoCRegisterGameOperation();
+            var cmd1 = new Mock<ICommand>();
+
+            reg.Execute();
+            var game = Ioc.Resolve<Game>("Game.Receiver");
+
+            game.Receive(cmd1.Object);
+            game.Execute();
+
+            cmd1.Verify(cmd => cmd.Execute(), Times.Once);
+        }
+        public void Dispose()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+        }
+    }
+}
