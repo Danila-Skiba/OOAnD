8069bb05e0728d9cbe24770adcc8cfc5f39d085a
no7yao <dad62482@gmail.com>
2025-03-05 05:49:13 +0300

    feat: add IoCRegisterGameOperation.cs

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/IoCRegisterGameOperation.cs
@@ -0,0 +1,19 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class IoCRegisterGameOperation : ICommand
+    {
+        public void Execute()
+        {
+            var game = new Game();
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Game.Receiver",
+                (object[] args) =>
+                {
+                    return game;
+                }).Execute();
+        }
+    }
+}
