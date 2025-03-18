943d905c567dc1caa0d4049a117523e8502c48e8
tatsivo <t-pudovkina@inbox.ru>
2025-03-19 03:39:12 +0700

    feat: RegisterIoCDependencyAuthCommand

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/RegisterIoCDependencyAuthCommandTests.cs
@@ -0,0 +1,37 @@
+﻿using App;
+using App.Scopes;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class RegisterAuthDependenciesTests : IDisposable
+    {
+        public RegisterAuthDependenciesTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        public void Dispose()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+        }
+
+        [Fact]
+        public void Execute_RegistersAuthCommand()
+        {
+            var registerAuthDependencies = new RegisterAuthDependencies();
+
+            registerAuthDependencies.Execute();
+
+            var authCommand = Ioc.Resolve<ICommand>(
+                "Commands.Auth",
+                "player1",
+                "ship1",
+                "Fire"
+            );
+
+            Assert.IsType<AuthCommand>(authCommand);
+        }
+    }
+}
