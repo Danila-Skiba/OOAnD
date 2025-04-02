294b1356565a0dd3cb5aeeeaf7420b98830c1d23
Skripa Andrey <stefan.richard.sk@gmail.com>
2025-04-02 10:09:07 +0600

    feat: add IoCRegisterGameOperation and GameReceiver

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/GameReceiver.cs
@@ -0,0 +1,13 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class GameReceiver : ICommandReceiver
+    {
+        public void Receive(ICommand? cmd)
+        {
+            var queue = Ioc.Resolve<Queue<ICommand>>("Game.CommandsQueue");
+            queue.Enqueue(cmd);
+        }
+    }
+}
