996de7949df85e6876bc29cc35df8e2424fdf9df
Skripa Andrey <stefan.richard.sk@gmail.com>
2025-04-02 10:09:33 +0600

    test: add IoCRegisterGameOperatioTests and GameReceiverTests

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/IoCRegisterGameOperationTests.cs
@@ -0,0 +1,31 @@
+﻿using App;
+using App.Scopes;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class IoCRegisterGameOperationTests : IDisposable
+    {
+        public IoCRegisterGameOperationTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void Execute_RegistersGameReceiver()
+        {
+            var operation = new IoCRegisterGameOperation();
+            operation.Execute();
+
+            var receiver = Ioc.Resolve<ICommandReceiver>("Game.Receiver");
+            Assert.NotNull(receiver);
+            Assert.IsType<GameReceiver>(receiver);
+        }
+
+        public void Dispose()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+        }
+    }
+}
