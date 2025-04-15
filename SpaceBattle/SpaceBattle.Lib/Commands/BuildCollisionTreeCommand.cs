382a092a25c8584a9d0e0fc0fcb1256c497f95c6
Smolovaia Zhanna <zs_e7@mail.ru>
2025-04-15 15:47:22 +0600

    feat: add tree building logic

--- SpaceBattle/SpaceBattle.Lib/Commands/BuildCollisionTreeCommand.cs
+++ SpaceBattle/SpaceBattle.Lib/Commands/BuildCollisionTreeCommand.cs
@@ -4,42 +4,17 @@ namespace SpaceBattle.Lib
 {
     public class BuildCollisionTreeCommand : ICommand
     {
-        private readonly ICollisionTreeDataProvider _provider;
+        private readonly ICollisionTreeDataProvider provider;
+
         public BuildCollisionTreeCommand(ICollisionTreeDataProvider provider)
         {
-            _provider = provider;
+            this.provider = provider;
         }
         public void Execute()
         {
-            var vectors = _provider.GetCollisionVectors();
-            var tree = new Dictionary<int, object>();
-
-            vectors.ToList().ForEach(vector =>
-            {
-                if (vector == null)
-                {
-                    return;
-                }
-
-                var current = tree;
-
-                vector.Take(vector.Length - 1).ToList().ForEach(param =>
-                {
-                    if (!current.ContainsKey(param))
-                    {
-                        current[param] = new Dictionary<int, object>();
-                    }
-
-                    current = (Dictionary<int, object>)current[param];
-                });
-
-                var lastParam = vector.Last();
-                if (!current.ContainsKey(lastParam))
-                {
-                    current[lastParam] = new Dictionary<int, object>();
-                }
-            });
-            Ioc.Resolve<App.ICommand>("IoC.Register", "Game.Struct.CollisionTree", (object[] args) => tree).Execute();
+            var vectors = provider.GetVectors();
+            var tree = Ioc.Resolve<Dictionary<int, object>>("Game.Struct.CollisionTree");
+            CollisionTreeBuilder.Build(tree, vectors);
         }
     }
 }
