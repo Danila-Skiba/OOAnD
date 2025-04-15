edf89e61b03cfbd5e40054cadd063f4946c05ff1
Danila-Skiba <fiks11pikvik@gmail.com>
2025-04-15 23:10:03 +0600

    fix: CollisionCommand.cs, CollisionCommandTests.cs

--- SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/CollisionCommandTests.cs
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/CollisionCommandTests.cs
@@ -15,6 +15,12 @@ namespace SpaceBattle.Lib.Tests
                     }}
                 }}
             };
+
+        private readonly Dictionary<(string, string), string> collisionRules = new Dictionary<(string, string), string>
+        {
+            { ("Torpedo", "Ship"), "Ship" },
+            { ("Ship", "Asteroid"), "Ship" }
+        };
         public CollisionCommandTests()
         {
             new InitCommand().Execute();
@@ -32,12 +38,27 @@ namespace SpaceBattle.Lib.Tests
                 var obj = (IDictionary<string, object>)args[0];
                 return (int[])obj["Velocity"];
             }).Execute();
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Game.GetType", (object[] args) =>
+            {
+                var obj = (IDictionary<string, object>)args[0];
+                return (string)obj["Type"];
+            }).Execute();
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Get.Collision.ReferenceRules", (object[] args) =>
+            {
+                return collisionRules;
+            }).Execute();
+
+            var registerCollDependencies = new RegiseterCollisionDependencies();
+
+            registerCollDependencies.Execute();
         }
         [Fact]
         public void CollisionCommandTrueTest()
         {
             //Arrange
-            Init_CollisionTree(collisionTree);
+            Init_CollisionTree(collisionTree, "ShipTorpedo");
             var collisionHandle = new Mock<ICommand>();
             collisionHandle.Setup(c => c.Execute());
 
@@ -51,6 +72,9 @@ namespace SpaceBattle.Lib.Tests
             obj1.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
             obj2.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
 
+            obj1.Setup(obj => obj["Type"]).Returns("Torpedo");
+            obj2.Setup(obj => obj["Type"]).Returns("Ship");
+
             var commandCollision = new CollisionCommand(obj1.Object, obj2.Object);
             //Act
             commandCollision.Execute();
@@ -63,7 +87,7 @@ namespace SpaceBattle.Lib.Tests
         {
 
             //Arrange
-            Init_CollisionTree(collisionTree);
+            Init_CollisionTree(collisionTree, "ShipTorpedo");
             var collisionHandle = new Mock<ICommand>();
             collisionHandle.Setup(c => c.Execute());
 
@@ -77,6 +101,9 @@ namespace SpaceBattle.Lib.Tests
             obj1.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 3 });
             obj2.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
 
+            obj1.Setup(obj => obj["Type"]).Returns("Torpedo");
+            obj2.Setup(obj => obj["Type"]).Returns("Ship");
+
             var commandCollision = new CollisionCommand(obj1.Object, obj2.Object);
             //Act
             commandCollision.Execute();
@@ -85,45 +112,14 @@ namespace SpaceBattle.Lib.Tests
         }
 
         [Fact]
-        public void CollisionCommandExceptionRange()
-        {
-            //Arrange
-            Init_CollisionTree(collisionTree);
-            var collisionHandle = new Mock<ICommand>();
-            collisionHandle.Setup(c => c.Execute());
-
-            Ioc.Resolve<App.ICommand>("IoC.Register", "Collision.Handle", (object[] args) => collisionHandle.Object).Execute();
-
-            var obj1 = new Mock<IDictionary<string, object>>();
-            var obj2 = new Mock<IDictionary<string, object>>();
-
-            obj1.Setup(obj => obj["Position"]).Returns(new int[] { 0 });
-            obj2.Setup(obj => obj["Position"]).Returns(new int[] { 0, 0 });
-
-            obj1.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 3 });
-            obj2.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
-
-            var commandCollision = new CollisionCommand(obj1.Object, obj2.Object);
-
-            //Act & Assert
-            Assert.Throws<IndexOutOfRangeException>(() => commandCollision.Execute());
-            collisionHandle.Verify(c => c.Execute(), Times.Never());
-
-        }
-
-        [Fact]
         public void CollisionCommandException_NonValideCollisionTree()
         {
             var newCollisionTree = new Dictionary<int, object>(){
-                {1,new Dictionary<int, object>(){
-                    {1,new Dictionary<int, object>(){
-                        {0, new Dictionary<int, object>(){
-                            {0, "end"}
-                        }}
-                    }}
+                { 0, new Dictionary<int, object>(){
+                    {0, "not a dictionary"}
                 }}
             };
-            Init_CollisionTree(newCollisionTree);
+            Init_CollisionTree(newCollisionTree, "ShipTorpedo");
 
             var collisionHandle = new Mock<ICommand>();
             collisionHandle.Setup(c => c.Execute());
@@ -139,18 +135,21 @@ namespace SpaceBattle.Lib.Tests
             obj1.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 3 });
             obj2.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
 
-            var commandCollision = new CollisionCommand(obj1.Object, obj2.Object);
+            obj1.Setup(obj => obj["Type"]).Returns("Torpedo");
+            obj2.Setup(obj => obj["Type"]).Returns("Ship");
 
+            var commandCollision = new CollisionCommand(obj1.Object, obj2.Object);
+            //Act
             commandCollision.Execute();
-            //Act & Assert
+            // Assert
             collisionHandle.Verify(c => c.Execute(), Times.Never());
         }
 
         [Fact]
-        public void CollisionCommandExceptionRangeVelocity()
+        public void CollisionCommand_True_ReversePairRule()
         {
-            //Arrange
-            Init_CollisionTree(collisionTree);
+            // Arrange
+            Init_CollisionTree(collisionTree, "ShipTorpedo");
             var collisionHandle = new Mock<ICommand>();
             collisionHandle.Setup(c => c.Execute());
 
@@ -159,18 +158,84 @@ namespace SpaceBattle.Lib.Tests
             var obj1 = new Mock<IDictionary<string, object>>();
             var obj2 = new Mock<IDictionary<string, object>>();
 
-            obj1.Setup(obj => obj["Position"]).Returns(new int[] { 0, 0 });
+            obj1.Setup(obj => obj["Position"]).Returns(new int[] { 1, 1 });
             obj2.Setup(obj => obj["Position"]).Returns(new int[] { 0, 0 });
 
-            obj1.Setup(obj => obj["Velocity"]).Returns(new int[] { 1 });
+            obj1.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
             obj2.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
+
+            obj1.Setup(obj => obj["Type"]).Returns("Ship");
+            obj2.Setup(obj => obj["Type"]).Returns("Torpedo");
 
             var commandCollision = new CollisionCommand(obj1.Object, obj2.Object);
 
-            //Act & Assert
-            Assert.Throws<IndexOutOfRangeException>(() => commandCollision.Execute());
-            collisionHandle.Verify(c => c.Execute(), Times.Never());
+            // Act
+            commandCollision.Execute();
 
+            // Assert
+            collisionHandle.Verify(c => c.Execute(), Times.Once());
+        }
+
+        [Fact]
+        public void CollisionCommand_True_NotRules()
+        {
+            // Arrange
+            Init_CollisionTree(collisionTree, "AsteroidTorpedo");
+            var collisionHandle = new Mock<ICommand>();
+            collisionHandle.Setup(c => c.Execute());
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Collision.Handle", (object[] args) => collisionHandle.Object).Execute();
+
+            var obj1 = new Mock<IDictionary<string, object>>();
+            var obj2 = new Mock<IDictionary<string, object>>();
+
+            obj1.Setup(obj => obj["Position"]).Returns(new int[] { 1, 1 });
+            obj2.Setup(obj => obj["Position"]).Returns(new int[] { 0, 0 });
+
+            obj1.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
+            obj2.Setup(obj => obj["Velocity"]).Returns(new int[] { 1, 1 });
+
+            obj1.Setup(obj => obj["Type"]).Returns("Asteroid");
+            obj2.Setup(obj => obj["Type"]).Returns("Torpedo");
+
+            var commandCollision = new CollisionCommand(obj1.Object, obj2.Object);
+
+            // Act
+            commandCollision.Execute();
+
+            // Assert
+            collisionHandle.Verify(c => c.Execute(), Times.Once());
+        }
+
+        [Fact]
+        public void CollisionCommand_False_NullParams()
+        {
+            // Arrange
+            Init_CollisionTree(collisionTree, "ShipTorpedo");
+            var collisionHandle = new Mock<ICommand>();
+            collisionHandle.Setup(c => c.Execute());
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Collision.Handle", (object[] args) => collisionHandle.Object).Execute();
+
+            var obj1 = new Mock<IDictionary<string, object>>();
+            var obj2 = new Mock<IDictionary<string, object>>();
+
+            obj1.Setup(obj => obj["Position"]).Returns(new int[] { });
+            obj2.Setup(obj => obj["Position"]).Returns(new int[] { });
+
+            obj1.Setup(obj => obj["Velocity"]).Returns(new int[] { });
+            obj2.Setup(obj => obj["Velocity"]).Returns(new int[] { });
+
+            obj1.Setup(obj => obj["Type"]).Returns("Torpedo");
+            obj2.Setup(obj => obj["Type"]).Returns("Ship");
+
+            var commandCollision = new CollisionCommand(obj1.Object, obj2.Object);
+
+            // Act
+            commandCollision.Execute();
+
+            // Assert
+            collisionHandle.Verify(c => c.Execute(), Times.Never());
         }
 
         public void Dispose()
@@ -178,9 +243,9 @@ namespace SpaceBattle.Lib.Tests
             Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
         }
 
-        private static void Init_CollisionTree(IDictionary<int, object> collisionTree)
+        private static void Init_CollisionTree(IDictionary<int, object> collisionTree, string treeKey)
         {
-            Ioc.Resolve<App.ICommand>("IoC.Register", "Game.CollisionTree", (object[] args) => collisionTree).Execute();
+            Ioc.Resolve<App.ICommand>("IoC.Register", $"Game.CollisionTree.{treeKey}", (object[] args) => collisionTree).Execute();
         }
     }
 }
--- SpaceBattle/SpaceBattle.Lib/Commands/CollisionCommand.cs
+++ SpaceBattle/SpaceBattle.Lib/Commands/CollisionCommand.cs
@@ -15,22 +15,11 @@ namespace SpaceBattle.Lib
 
         public void Execute()
         {
-            var position1 = Ioc.Resolve<int[]>("Game.GetPosition", _obj1);
-            var position2 = Ioc.Resolve<int[]>("Game.GetPosition", _obj2);
+            var values = Ioc.Resolve<(int[] branch, string treeKey)>("Get.Collision.State", _obj1, _obj2);
 
-            var velocity1 = Ioc.Resolve<int[]>("Game.GetVelocity", _obj1);
-            var velocity2 = Ioc.Resolve<int[]>("Game.GetVelocity", _obj2);
+            var collisionTree = Ioc.Resolve<IDictionary<int, object>>($"Game.CollisionTree.{values.treeKey}");
 
-            var dPositions = position2.ToList().Select((value, index) => value - position1[index]);
-            var dVelocities = velocity2.ToList().Select((value, index) => value - velocity1[index]);
-
-            var branch = dPositions.Concat(dVelocities).ToArray();
-
-            var collisionTree = Ioc.Resolve<IDictionary<int, object>>("Game.CollisionTree");
-
-            var collision = CheckCollision(collisionTree, branch);
-
-            if (collision)
+            if (CheckCollision(collisionTree, values.branch))
             {
                 Ioc.Resolve<ICommand>("Collision.Handle", _obj1, _obj2).Execute();
             }
@@ -39,6 +28,11 @@ namespace SpaceBattle.Lib
         private static bool CheckCollision(IDictionary<int, object> collTree, int[] parameters)
         {
             object current = collTree;
+            if (parameters.Length == 0)
+            {
+                return false;
+            }
+
             return parameters.All(param =>
             {
                 if (current is IDictionary<int, object> dict && dict.TryGetValue(param, out var next))
