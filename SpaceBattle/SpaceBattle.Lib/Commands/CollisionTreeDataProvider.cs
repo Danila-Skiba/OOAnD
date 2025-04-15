5fde7a46c00e40081b15f707013d5a509151eeaa
Smolovaia Zhanna <zs_e7@mail.ru>
2025-04-15 15:45:36 +0600

    feat: add classes for loading collision vectors

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/CollisionTreeDataProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceBattle.Lib
+{
+    public class CollisionTreeDataProvider : ICollisionTreeDataProvider
+    {
+        private readonly List<int[]> vectors;
+
+        public CollisionTreeDataProvider(List<int[]> vectors)
+        {
+            this.vectors = vectors;
+        }
+        public List<int[]> GetVectors()
+        {
+            return vectors;
+        }
+    }
+}
