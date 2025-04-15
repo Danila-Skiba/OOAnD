d4827482d6a460402cb7838564154897d28b3699
Smolovaia Zhanna <zs_e7@mail.ru>
2025-04-15 15:49:46 +0600

    test: add tests for collision tree and data providers

--- SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/BuildCollisionTreeCommandTest.cs
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/BuildCollisionTreeCommandTest.cs
@@ -12,75 +12,87 @@ namespace SpaceBattle.Lib.Tests.CommandTests
             var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
             Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
         }
-        [Fact]
-        public void BuildCollisionTreeWithLength4()
-        {
-            var providerMock = new Mock<ICollisionTreeDataProvider>();
-            providerMock.Setup(p => p.GetCollisionVectors()).Returns(new List<int[]?>
-            {
-                new [] { 1, 1, -1, -1},
-                new [] { 1, 1, 0, 0},
-                new [] { 2, 2, 1, 1}
-            });
-
-            var buildcommand = new BuildCollisionTreeCommand(providerMock.Object);
-            buildcommand.Execute();
-
-            var tree = Ioc.Resolve<IDictionary<int, object>>("Game.Struct.CollisionTree");
-            Assert.NotNull(tree);
-
-            var level1 = (IDictionary<int, object>)tree[1];
-            var level2 = (IDictionary<int, object>)level1[1];
-            var level3 = (IDictionary<int, object>)level2[-1];
-            var level4 = (IDictionary<int, object>)level3[-1];
-            Assert.IsType<Dictionary<int, object>>(level4);
-
-            level3 = (IDictionary<int, object>)level2[0];
-            level4 = (IDictionary<int, object>)level3[0];
-            Assert.IsType<Dictionary<int, object>>(level4);
-
-            level1 = (IDictionary<int, object>)tree[2];
-            level2 = (IDictionary<int, object>)level1[2];
-            level3 = (IDictionary<int, object>)level2[1];
-            level4 = (IDictionary<int, object>)level3[1];
-            Assert.IsType<Dictionary<int, object>>(level4);
-        }
 
         [Fact]
-        public void BuildCollisionTreeCommandWithInvalidVectors()
+        public void BuildCollisionTreeFromFile()
         {
+            var treeInstance = new Dictionary<int, object>();
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Game.Struct.CollisionTree", (object[] args) => treeInstance).Execute();
+
+            var treeBefore = Ioc.Resolve<Dictionary<int, object>>("Game.Struct.CollisionTree");
+            Assert.Empty(treeBefore);
+
+            var filePath = "../../../testTree.txt";
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "CollisionDataProvider.FromFile", (object[] args) => new FileCollisionTreeDataProvider((string)args[0])).Execute();
+            var provider = Ioc.Resolve<ICollisionTreeDataProvider>("CollisionDataProvider.FromFile", filePath);
+            var vectors = provider.GetVectors();
+
+            Assert.NotEmpty(vectors);
+            Assert.Contains(vectors, v => v.Length > 0 && v[0] == 1);
+
             var providerMock = new Mock<ICollisionTreeDataProvider>();
-            providerMock.Setup(p => p.GetCollisionVectors()).Returns(new List<int[]?>
-            {
-                null,
-                new [] { 1, 1, -1, -1}
-            });
-
-            var buildcommand = new BuildCollisionTreeCommand(providerMock.Object);
-            buildcommand.Execute();
-
-            var tree = Ioc.Resolve<IDictionary<int, object>>("Game.Struct.CollisionTree");
-            Assert.NotNull(tree);
-
-            var level1 = (IDictionary<int, object>)tree[1];
-            var level2 = (IDictionary<int, object>)level1[1];
-            var level3 = (IDictionary<int, object>)level2[-1];
-            var level4 = (IDictionary<int, object>)level3[-1];
-            Assert.IsType<Dictionary<int, object>>(level4);
-        }
-
-        [Fact]
-        public void BuildCollisionTreeCommandEmptyVectors()
-        {
-            var providerMock = new Mock<ICollisionTreeDataProvider>();
-            providerMock.Setup(p => p.GetCollisionVectors()).Returns(new List<int[]?>());
+            providerMock.Setup(p => p.GetVectors()).Returns(vectors);
 
             var buildCommand = new BuildCollisionTreeCommand(providerMock.Object);
             buildCommand.Execute();
 
-            var tree = Ioc.Resolve<IDictionary<int, object>>("Game.Struct.CollisionTree");
+            var tree = Ioc.Resolve<Dictionary<int, object>>("Game.Struct.CollisionTree");
             Assert.NotNull(tree);
-            Assert.Empty(tree);
+
+            Assert.Contains(1, tree.Keys);
+            var level1_1 = (Dictionary<int, object>)tree[1];
+            Assert.Contains(2, level1_1.Keys);
+            var level2_1 = (Dictionary<int, object>)level1_1[2];
+            Assert.Contains(3, level2_1.Keys);
+            var level3_1 = (Dictionary<int, object>)level2_1[3];
+            Assert.Contains(4, level3_1.Keys);
+
+            var level2_2 = (Dictionary<int, object>)level1_1[2];
+            Assert.Contains(0, level2_2.Keys);
+            var level3_2 = (Dictionary<int, object>)level2_2[0];
+            Assert.Contains(0, level3_2.Keys);
+
+            Assert.Contains(2, tree.Keys);
+            var level1_3 = (Dictionary<int, object>)tree[2];
+            Assert.Contains(2, level1_3.Keys);
+            var level2_3 = (Dictionary<int, object>)level1_3[2];
+            Assert.Contains(1, level2_3.Keys);
+            var level3_3 = (Dictionary<int, object>)level2_3[1];
+            Assert.Contains(1, level3_3.Keys);
+        }
+
+        [Fact]
+        public void BuildCollisionTreeCommandFailsToReadFile()
+        {
+            var invalidFilePath = "nonexistent_file.txt";
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "CollisionDataProvider.FromFile", (object[] args) => new FileCollisionTreeDataProvider((string)args[0])).Execute();
+            var provider = Ioc.Resolve<ICollisionTreeDataProvider>("CollisionDataProvider.FromFile", invalidFilePath);
+
+            var buildCommand = new BuildCollisionTreeCommand(provider);
+
+            var exception = Assert.Throws<FileNotFoundException>(() => buildCommand.Execute());
+            Assert.Contains("nonexistent_file.txt", exception.Message);
+        }
+
+        [Fact]
+        public void CollisionTreeDataProviderReturnsGivenVectors()
+        {
+            var filePath = "../../../testTree.txt";
+
+            var inputVectors = File.ReadLines(filePath)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToArray())
+                .ToList();
+
+            Ioc.Resolve<App.ICommand>("IoC.Register", "CollisionDataProvider.FromMemory", (object[] args) => new CollisionTreeDataProvider((List<int[]>)args[0])).Execute();
+            var provider = Ioc.Resolve<ICollisionTreeDataProvider>("CollisionDataProvider.FromMemory", inputVectors);
+            var result = provider.GetVectors();
+
+            Assert.Equal(inputVectors, result);
         }
 
         public void Dispose()
