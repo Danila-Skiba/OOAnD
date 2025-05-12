0c0cf6f26c64c906eefa99e1214241215cafb462
Smolovaia Zhanna <zs_e7@mail.ru>
2025-05-12 14:44:26 +0600

    test: add tests for adapter generator

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/GenerationTests.cs
@@ -0,0 +1,60 @@
+﻿using App;
+using App.Scopes;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class GenerationTests : IDisposable
+    {
+        public GenerationTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void GenerateAdapter_ReturnsCorrectString()
+        {
+            new AdaptersGenerator().Execute();
+
+            var generatedCode = Ioc.Resolve<string>("Game.Adapters.Generate", typeof(IMoving));
+
+            var expected = @"
+public class IMovingAdapter : IMoving
+{
+    private readonly IDictionary<string, object> _dict;
+
+    public IMovingAdapter(IDictionary<string, object> dict)
+    {
+        _dict = dict;
+    }
+
+    public SpaceBattle.Lib.Vector Position
+    {
+        get
+        {
+            return Ioc.Resolve<SpaceBattle.Lib.Vector>(""Object.GetProperty"", _dict, ""Position"", typeof(SpaceBattle.Lib.Vector));
+        }
+        set
+        {
+            Ioc.Resolve<ICommand>(""Object.SetProperty"", _dict, ""Position"", value).Execute();
+        }
+    }
+    public SpaceBattle.Lib.Vector Velocity
+    {
+        get
+        {
+            return Ioc.Resolve<SpaceBattle.Lib.Vector>(""Object.GetProperty"", _dict, ""Velocity"", typeof(SpaceBattle.Lib.Vector));
+        }
+    }
+}";
+
+            Assert.Equal(expected.Trim(), generatedCode.Trim());
+        }
+
+        public void Dispose()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+        }
+    }
+}
