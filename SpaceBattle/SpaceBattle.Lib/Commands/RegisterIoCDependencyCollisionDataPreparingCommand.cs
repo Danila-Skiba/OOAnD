35f795d1c0cba4b41a432cee8d9793a2eb4d1cbe
no7yao <dad62482@gmail.com>
2025-05-14 00:19:54 +0300

    feat: add Registration IoC dependencies for CollisionDataPreparingCommand and SaveCollisionDataToFileCommand

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterIoCDependencyCollisionDataPreparingCommand.cs
@@ -0,0 +1,13 @@
+﻿using App;
+namespace SpaceBattle.Lib;
+public class RegisterIoCDependencyCollisionDataPreparingCommand : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.DataPreparing",
+            (object[] args) => new CollisionDataPreparingCommand((ICollisionDataGenerator)args[0])
+        ).Execute();
+    }
+}
---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterIoCDependencySaveCollisionDataToFileCommand.cs
@@ -0,0 +1,14 @@
+﻿using App;
+
+namespace SpaceBattle.Lib;
+public class RegisterIoCDependencySaveCollisionDataToFileCommand : ICommand
+{
+    public void Execute()
+    {
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Collision.SaveDataToFile",
+            (object[] args) => new SaveCollisionDataToFileCommand((string)args[0], (IList<int[]>)args[1])
+        ).Execute();
+    }
+}
