88e3ce574e32f49dca5fbc0ee5afe971d0687969
no7yao <dad62482@gmail.com>
2025-05-14 00:25:31 +0300

    test: add RegisterIoCDependencyCollisionDataPreparingCommandTests.cs and RegisterIoCDependencySaveCollisionDataToFileCommandTests.cs

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/RegisterIoCDependencyCollisionDataPreparingCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class RegisterIoCDependencyCollisionDataPreparingCommandTests : IDisposable
+    {
+        public RegisterIoCDependencyCollisionDataPreparingCommandTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void RegisterIoCDependencyCollisionDataPreparingCommandRegistersCorrectlyTest()
+        {
+            var mockGenerator = new Mock<ICollisionDataGenerator>();
+
+            new RegisterIoCDependencyCollisionDataPreparingCommand().Execute();
+            var command = Ioc.Resolve<ICommand>("Collision.DataPreparing", mockGenerator.Object);
+
+            Assert.IsType<CollisionDataPreparingCommand>(command);
+        }
+
+        public void Dispose()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+        }
+    }
+}
