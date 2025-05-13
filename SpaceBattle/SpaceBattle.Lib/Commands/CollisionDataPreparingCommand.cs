7c7290115679e2a5d4f16270d16a8742a91b1438
no7yao <dad62482@gmail.com>
2025-05-14 00:09:51 +0300

    feat: add CollisionDataPreparingCommand.cs

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/CollisionDataPreparingCommand.cs
@@ -0,0 +1,21 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class CollisionDataPreparingCommand : ICommand
+    {
+        private readonly ICollisionDataGenerator generator;
+        public CollisionDataPreparingCommand(ICollisionDataGenerator generator)
+        {
+            this.generator = generator;
+        }
+
+        public void Execute()
+        {
+            var vectors = generator.GenerateCollisionData();
+            var fileName = $"{generator.object1}_{generator.object2}.txt";
+            var saveCommand = Ioc.Resolve<ICommand>("Collision.SaveDataToFile", fileName, vectors);
+            saveCommand.Execute();
+        }
+    }
+}
