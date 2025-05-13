72e26c3b232208eab2339810dc755ff85f388fcc
no7yao <dad62482@gmail.com>
2025-05-14 00:16:34 +0300

    feat: add SaveCollisionDataToFileCommand.cs

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/SaveCollisionDataToFileCommand.cs
@@ -0,0 +1,27 @@
+﻿using App;
+
+namespace SpaceBattle.Lib;
+public class SaveCollisionDataToFileCommand : ICommand
+{
+    private readonly string fileName;
+    private readonly IList<int[]> vectors;
+
+    public SaveCollisionDataToFileCommand(string fileName, IList<int[]> vectors)
+    {
+        this.fileName = fileName;
+        this.vectors = vectors;
+    }
+
+    public void Execute()
+    {
+        var basePath = Ioc.Resolve<string>("Data.FilePath");
+        var fullPath = Path.Combine(basePath, fileName);
+        var lines = new List<string>();
+        foreach (var vector in vectors)
+        {
+            lines.Add(string.Join(" ", vector));
+        }
+
+        File.WriteAllLines(fullPath, lines);
+    }
+}
