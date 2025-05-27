e03b97b42249daaa33f9336ebf111da808d6077e
Skripa Andrey <stefan.richard.sk@gmail.com>
2025-05-27 12:32:15 +0600

    feat: add RegisterAdapterCommand.cs

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterCommandDependency.cs
@@ -0,0 +1,30 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class RegisterAdapterCommand<TInterface, TCommand> : ICommand
+        where TCommand : class
+    {
+        private readonly string _key;
+        public RegisterAdapterCommand(string key)
+        {
+            _key = key;
+        }
+
+        public void Execute()
+        {
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register", _key,
+                (object[] args) =>
+                {
+                    var objDict = (IDictionary<string, object>)args[0];
+
+                    var service = (TInterface)Ioc.Resolve<object>(
+                        "Game.Adapter.Create", objDict, typeof(TInterface));
+
+                    return Activator.CreateInstance(typeof(TCommand), service)!;
+                })
+            .Execute();
+        }
+    }
+}
