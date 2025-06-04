9ab8d8339c12164669bda252470932a43c6825a1
no7yao <dad62482@gmail.com>
2025-06-04 20:20:25 +0300

    feat: add RegisterIoCDependencyCollisionDataGenerator.cs

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterIoCDependencyCollisionDataGenerator.cs
@@ -0,0 +1,18 @@
+﻿using App;
+
+namespace SpaceBattle.Lib
+{
+    public class RegisterIoCDependencyCollisionDataGenerator : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Collision.DataGenerator",
+                (object[] args) => new CollisionDataGenerator(
+                    (string)args[0],
+                    (string)args[1])
+            ).Execute();
+        }
+    }
+}
